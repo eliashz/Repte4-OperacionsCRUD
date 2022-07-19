adeadd4d107bacdff474aea85faa35e2940ab2f9
eliashz <eliashz@gmail.com>
2022-07-19 12:42:52 +0200

    create controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,12 +3,29 @@ const User = db.users;
 
 // Find all models
 exports.findAll = (req, res) => {
-
 };
 
-//Create model
+// Create model
 exports.create = (req, res) => {
-
+    if (!req.body.username) {
+        return res.status(404).send({ message: "Content can not be empty."})
+    }
+    // Create model
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email
+    });
+    // Save in database
+    user
+        .save(user)
+        .then(data => {
+            res.status(201).send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating a user."
+            });
+        });
 };
 
 // Find one model by ID
